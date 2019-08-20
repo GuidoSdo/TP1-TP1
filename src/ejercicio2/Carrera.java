0a9b1ddf5a04af1ae2c6e07cb5fc66553f2d944f
guidoHome <guido.salcedo96@gmail.com>
2019-08-20 01:05:13 -0300

    Empezo el ejercicio 2

---
+++ src/ejercicio2/Carrera.java
@@ -0,0 +1,20 @@
+package ejercicio2;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Carrera {
+
+    ArrayList<Atleta> atletas;
+    ArrayList<TipoEspecialidad> especialidades;
+    Atleta atletaGanador;
+    float segundosMasCorto;
+
+
+    public Carrera() {
+    }
+
+    public void agregarAtleta(Atleta atleta, float segundosRecorridos){
+
+    }
+}
